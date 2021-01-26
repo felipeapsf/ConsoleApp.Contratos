91d316936c25a5c5dfc9ccfcec3764e90ae0bba5
Felipe Fidalgo <felipe_apsf@hotmail.com>
2021-01-26 11:18:38 -0300

    Add project files.

---
+++ ConsoleApp.Contratos/Services/IOnlinePaymentService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp.Contratos.Services
+{
+    interface IOnlinePaymentService
+    {
+        public double PaymentFee(double amount);
+        public double Interest(double amount, int months);
+    }
+}
